5c520ef9e4611ff31bf41c3097c13293f8eead01
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-01-26 00:48:56 -0300

    Setting up sass and bulma

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 
 function App () {
 	return (
 		<div className='App'>
-			<h1>hello world</h1>
+			<h1 className='title'>Bulma</h1>
+			<p className='subtitle'>
+				Modern CSS framework based on{' '}
+				<a href='https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox'>
+					Flexbox
+				</a>
+			</p>
+
+			<div className='field'>
+				<div className='control'>
+					<input className='input' type='text' placeholder='Input' />
+				</div>
+			</div>
+
+			<div className='field'>
+				<p className='control'>
+					<span className='select'>
+						<select>
+							<option>Select dropdown</option>
+						</select>
+					</span>
+				</p>
+			</div>
+
+			<div className='buttons'>
+				<a className='button is-primary'>Primary</a>
+				<a className='button is-link'>Link</a>
+			</div>
 		</div>
 	)
 }
