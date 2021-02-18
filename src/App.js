e2a3ed8aa3c3e00690fd2c35b9ce3929ba06363d
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-02-18 00:16:54 -0300

    Palette changes and adding an icon to the header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './App.scss'
 
 import Header from './components/layout/Header'
 import Movie from './components/Movie'
 import Tutorial from './components/Tutorial'
+import useMovieData from './hooks/useMovieData'
 
 function App() {
+  const {
+    firstMovieData,
+    secondMovieData,
+    setFirstMovieData,
+    setSecondMovieData,
+    achievementsResults,
+  } = useMovieData()
+
+  const [showWarning, setShowWarning] = useState(false)
+
+  const areSameMovie = () => {
+    if (firstMovieData.id !== undefined && secondMovieData.id !== undefined) {
+      return firstMovieData.id === secondMovieData.id
+    }
+    return false
+  }
+  useEffect(() => {
+    setShowWarning(areSameMovie())
+  }, [firstMovieData, secondMovieData])
   return (
     <>
       <Header />
       <div className='container'>
+        {showWarning && (
+          <div className='notification is-link'>
+            {!areSameMovie && (
+              <button
+                className='delete'
+                onClick={() => setShowWarning(false)}
+              />
+            )}
+            You're comparing the same movie
+          </div>
+        )}
         <div className='columns'>
-          <Movie />
-          <Movie />
+          <Movie
+            setMovieData={setFirstMovieData}
+            movieData={firstMovieData}
+            achievementsResults={achievementsResults}
+          />
+          <Movie
+            setMovieData={setSecondMovieData}
+            movieData={secondMovieData}
+            achievementsResults={achievementsResults}
+          />
         </div>
-        <Tutorial />
+        {Object.keys(firstMovieData).length ||
+        Object.keys(secondMovieData).length ? (
+          ''
+        ) : (
+          <Tutorial />
+        )}
       </div>
     </>
   )
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { InlineIcon } from '@iconify/react-with-api'
 
 export default function Header() {
   return (
     <section className='hero is-primary is-bold'>
       <div className='hero-body'>
         <div className='container'>
-          <h1 className='title'>Movie Fight</h1>
+          <h1 className='title is-1 has-text-centered'>
+            Movie Fight <InlineIcon icon='emojione:admission-tickets' />
+          </h1>
         </div>
       </div>
     </section>
